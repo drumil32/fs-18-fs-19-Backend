bed06429dd5f58dc230a94c4d689cfee28c518dd
drumil32 <akheniad@gmail.com>
2024-08-27 23:04:35 +0530

    till 27-08-2024

--- 27-08-2024/loggingMiddleware.js
+++ 27-08-2024/loggingMiddleware.js
@@ -1,7 +1,9 @@
 export const loggingMiddleware = (req, res, next) => {
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
     console.log('From Middleware : Request is comming');
     console.log('Request Ip : ' + req.ip);
     console.log('Request Method : ' + req.method);
+    console.log(ip);
     next();
     console.log('From Middleware : Response is going');
 }
